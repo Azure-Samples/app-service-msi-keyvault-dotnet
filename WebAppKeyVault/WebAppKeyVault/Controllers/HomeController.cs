63881305257c25a9684bbe03be605c9f9252a1e7
Varun Sharma <varunsh@microsoft.com>
2017-09-07 17:24:33 -0700

    Add sample code

---
+++ WebAppKeyVault/WebAppKeyVault/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace WebAppKeyVault.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async System.Threading.Tasks.Task<ActionResult> Index()
+        {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+
+            try
+            {
+                var keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                var secret = await keyVaultClient.GetSecretAsync("https://appmsi5kv.vault.azure.net/secrets/secret")
+                    .ConfigureAwait(false);
+
+                ViewBag.Secret = $"Secret: {secret.Value}";
+                
+            }
+            catch (Exception exp)
+            {
+                ViewBag.Error = $"Something went wrong: {exp.Message}";
+            }
+
+            ViewBag.Principal = azureServiceTokenProvider.PrincipalUsed != null ? $"Principal Used: {azureServiceTokenProvider.PrincipalUsed}" : string.Empty;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebAppKeyVault/WebAppKeyVault/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppKeyVault.MvcApplication" Language="C#" %>
